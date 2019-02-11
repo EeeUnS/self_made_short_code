1f2f33ed5e4d2bf2a302f598147304312c09f68a
EeeUnS <42970615+EeeUnS@users.noreply.github.com>
2019-02-11 22:50:02 +0900

    Add files via upload

---
+++ class/string/Mystring.cpp
@@ -0,0 +1,101 @@
+#include"Mystring.hpp"
+#include<cstring>
+
+int String::num_Strings = 0;
+
+//static메소드
+int String::HowMany()
+{
+    return num_Strings;
+}
+//클래스 메소드
+
+String::String(const char *s)
+{
+    len = std::strlen(s);
+    str = new char[len+1];
+    std::strcpy(str, s);
+    num_Strings++;
+}
+
+String::String()
+{
+    len = 4;
+    str = new char[1];
+    str[0] = '\0';
+    num_Strings++;
+}
+
+String::String(const String &st)
+{
+    num_Strings++;
+    len=st.len;
+    str = new char[len+1];
+    std::strcpy(str, st.str);
+}
+
+String::~String()
+{
+    --num_Strings;
+    delete [] str;
+}
+//오버로드 연산자 메소드
+
+String & String::operator=(const String & st)
+{
+    if(this == &st)
+        return *this;
+    delete [] str;
+    len = st.len;
+    str = new char[len+1];
+    std::strcpy(str, st.str);
+    return *this;
+}
+
+String & String::operator=(const char *s)
+{
+    delete [] str;
+    len = std::strlen(s);
+    str = new char[len +1];
+    std::strcpy(str, s);
+    return *this;
+}
+
+char & String::operator[] (int i)
+{
+    return str[i];
+}
+
+const char & String::operator[] (int i) const
+{
+    return str[i];
+}
+
+     bool operator<(const String &st1, const String &st2)
+     {
+         return (std::strcmp(st1.str, st2.str) < 0);
+     }
+     bool operator>(const String &st1, const String &st2)
+     {
+         return st1.str >st2.str;
+     }
+     bool operator==(const String &st1, const String &st2)
+     {
+         return (std::strcmp(st1.str, st2.str) == 0);
+     }
+     ostream & operator<<(ostream & os, const String &st)
+     {
+         os << st.str;
+         return os;
+     }
+     istream & operator>>(istream & is, String &st)
+     {
+            char temp[String::CINLIN];
+            is.get(temp, String::CINLIN);
+            is.get(temp, String::CINLIN);      
+            if(is)
+                st = temp;
+            while(is && is.get() !='\n')
+                continue;
+            return is;
+     }
