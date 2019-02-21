9c9a6a4f883ea51a8d03dad05c56ec05d03f49c5
EeeUnS <42970615+EeeUnS@users.noreply.github.com>
2019-02-21 23:42:35 +0900

    Update binary_multiply.c

--- binary_multiply.c
+++ binary_multiply.c
@@ -2,20 +2,20 @@
 
 int main()
 {
-	  char  a = 10, b = 10; // a*b =c
-	 char c = 0;
-	 char n=0;
-	printf("%d \n%d" ,a ,b );
-	while(b != 0)
+	char a = 10, b = 10; // a*b =c
+	char c = 0;
+	char n = 0;
+	printf("%d \n%d\n", a, b);
+	while (b != 0)
 	{
 		n = b & 1;
-		b>> 1;
-		a<< 1;
-		if(n)
+		b = b >> 1;
+		if (n)
 		{
 			c += a;
 		}
-	}	
-	printf("%d" ,c);
-	return 0; 
+		a = a << 1;
+	}
+	printf("%d", c);
+	return 0;
 }
