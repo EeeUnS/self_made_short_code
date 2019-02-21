b7512d8fbcbfead3fef9c9f76b97b518c66ac8de
EeeUnS <42970615+EeeUnS@users.noreply.github.com>
2019-02-21 23:43:20 +0900

    Update binary_multiply_disasemble.s

--- binary_multiply_disasemble.s
+++ binary_multiply_disasemble.s
@@ -2,63 +2,78 @@
 
 int main()
 {
-003F6070  push        ebp  
-003F6071  mov         ebp,esp  
-003F6073  sub         esp,0F0h  
-003F6079  push        ebx  
-003F607A  push        esi  
-003F607B  push        edi  
-003F607C  lea         edi,[ebp-0F0h]  
-003F6082  mov         ecx,3Ch  
-003F6087  mov         eax,0CCCCCCCCh  
-003F608C  rep stos    dword ptr es:[edi]  
-003F608E  mov         ecx,offset _F6C3F367_소스.cpp (040A042h)  
-003F6093  call        @__CheckForDebuggerJustMyCode@4 (03E17D5h)  
-	char  a = 10, b = 10; // a*b =c
-003F6098  mov         byte ptr [a],0Ah  
-003F609C  mov         byte ptr [b],0Ah  
+00CD6070  push        ebp  
+00CD6071  mov         ebp,esp  
+00CD6073  sub         esp,0F0h  
+00CD6079  push        ebx  
+00CD607A  push        esi  
+00CD607B  push        edi  
+00CD607C  lea         edi,[ebp-0F0h]  
+00CD6082  mov         ecx,3Ch  
+00CD6087  mov         eax,0CCCCCCCCh  
+00CD608C  rep stos    dword ptr es:[edi]  
+00CD608E  mov         ecx,offset _F6C3F367_소스.cpp (0CEA042h)  
+00CD6093  call        @__CheckForDebuggerJustMyCode@4 (0CC17D5h)  
+	char a = 10, b = 10; // a*b =c
+00CD6098  mov         byte ptr [a],0Ah  
+00CD609C  mov         byte ptr [b],0Ah  
 	char c = 0;
-003F60A0  mov         byte ptr [c],0  
+00CD60A0  mov         byte ptr [c],0  
 	char n = 0;
-003F60A4  mov         byte ptr [n],0  
+00CD60A4  mov         byte ptr [n],0  
 	printf("%d \n%d", a, b);
-003F60A8  movsx       eax,byte ptr [b]  
-003F60AC  push        eax  
-003F60AD  movsx       ecx,byte ptr [a]  
-003F60B1  push        ecx  
-003F60B2  push        offset string "%d \n%d" (0401A08h)  
-003F60B7  call        _printf (03E10E6h)  
-003F60BC  add         esp,0Ch  
+00CD60A8  movsx       eax,byte ptr [b]  
+00CD60AC  push        eax  
+00CD60AD  movsx       ecx,byte ptr [a]  
+00CD60B1  push        ecx  
+00CD60B2  push        offset string "%d \n%d" (0CE1A08h)  
+00CD60B7  call        _printf (0CC10E6h)  
+00CD60BC  add         esp,0Ch  
 	while (b != 0)
-003F60BF  movsx       eax,byte ptr [b]  
-003F60C3  test        eax,eax  
-003F60C5  je          main+78h (03F60E8h)  
+00CD60BF  movsx       eax,byte ptr [b]  
+00CD60C3  test        eax,eax  
+00CD60C5  je          main+8Ah (0CD60FAh)  
 	{
 		n = b & 1;
-003F60C7  movsx       eax,byte ptr [b]  
-003F60CB  and         eax,1  
-003F60CE  mov         byte ptr [n],al  
-		b >> 1;
-		a << 1;
+00CD60C7  movsx       eax,byte ptr [b]  
+00CD60CB  and         eax,1  
+00CD60CE  mov         byte ptr [n],al  
+		b = b >> 1;
+00CD60D1  movsx       eax,byte ptr [b]  
+00CD60D5  sar         eax,1  
+00CD60D7  mov         byte ptr [b],al  
 		if (n)
-003F60D1  movsx       eax,byte ptr [n]  
-003F60D5  test        eax,eax  
-003F60D7  je          main+76h (03F60E6h)  
+00CD60DA  movsx       eax,byte ptr [n]  
+00CD60DE  test        eax,eax  
+00CD60E0  je          main+7Fh (0CD60EFh)  
 		{
 			c += a;
-003F60D9  movsx       eax,byte ptr [a]  
-003F60DD  movsx       ecx,byte ptr [c]  
-003F60E1  add         ecx,eax  
-003F60E3  mov         byte ptr [c],cl  
+00CD60E2  movsx       eax,byte ptr [a]  
+00CD60E6  movsx       ecx,byte ptr [c]  
+00CD60EA  add         ecx,eax  
+00CD60EC  mov         byte ptr [c],cl  
 		}
+		a = a << 1;
+00CD60EF  movsx       eax,byte ptr [a]  
+00CD60F3  shl         eax,1  
+00CD60F5  mov         byte ptr [a],al  
 	}
-003F60E6  jmp         main+4Fh (03F60BFh)  
+00CD60F8  jmp         main+4Fh (0CD60BFh)  
 	printf("%d", c);
-003F60E8  movsx       eax,byte ptr [c]  
-003F60EC  push        eax  
-003F60ED  push        offset string "%d" (0401A10h)  
-003F60F2  call        _printf (03E10E6h)  
-003F60F7  add         esp,8  
+00CD60FA  movsx       eax,byte ptr [c]  
+00CD60FE  push        eax  
+00CD60FF  push        offset string "%d" (0CE1A10h)  
+00CD6104  call        _printf (0CC10E6h)  
+00CD6109  add         esp,8  
 	return 0;
-003F60FA  xor         eax,eax  
+00CD610C  xor         eax,eax  
 }
+00CD610E  pop         edi  
+00CD610F  pop         esi  
+00CD6110  pop         ebx  
+00CD6111  add         esp,0F0h  
+00CD6117  cmp         ebp,esp  
+00CD6119  call        __RTC_CheckEsp (0CC1811h)  
+00CD611E  mov         esp,ebp  
+00CD6120  pop         ebp  
+00CD6121  ret  
