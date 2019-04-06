1e3eab0d23551dcfca9d8a82a2847a892b3d871f
EeeUnS <42970615+EeeUnS@users.noreply.github.com>
2019-04-07 03:20:15 +0900

    Add files via upload

---
+++ class/stack/stack.h
@@ -0,0 +1,60 @@
+#ifndef VECTOR__H
+#define VECTOR__H
+
+template<typename T>
+struct Node
+{
+	Node* pre;
+	T item;
+	Node* next;
+};
+
+template<typename T>
+class stack
+{
+public:
+	stack(const stack<T> &b); //iterator필요 미구현
+	stack();
+	stack(const T &item);
+	~stack();
+	void push(const T &item);
+	void pop();
+	int size();
+	bool empty();
+	T top();
+private:
+	size_t size_type;
+	Node<T>* end;
+public:
+	void swap(stack<T>& b);
+	bool operator== (const stack<T>& rhs); //iterator필요 미구현
+	bool operator!= (const stack<T>& rhs);//iterator필요 미구현
+	bool operator< (const stack<T>& rhs);
+	bool operator<= (const stack<T>& rhs);
+	bool operator> (const stack<T>& rhs);
+	bool operator>= (const stack<T>& rhs);
+
+};
+
+//
+//
+//template <class T, class Container>
+//bool operator== (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+//
+//template <class T, class Container>
+//bool operator!= (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+//
+//template <class T, class Container>
+//bool operator<  (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+//
+//template <class T, class Container>
+//bool operator<= (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+//
+//template <class T, class Container>
+//bool operator>  (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+//
+//template <class T, class Container>
+//bool operator>= (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+
+
+#endif // !1
