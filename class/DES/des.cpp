a772eea09e42f74a2e3771afcbe5c7bbfaf4e622
EeeUnS <42970615+EeeUnS@users.noreply.github.com>
2019-04-10 16:34:46 +0900

    전체적으로 완성 test X

--- class/DES/des.cpp
+++ class/DES/des.cpp
@@ -2,11 +2,11 @@
 #include<bitset>
 using namespace std;
 
-//index값이 실제 옮기는 값보다 1작음을 없애기 위해서 그냥 index는 1부터 시작
-//초기 P BOX
-static const int InitialPermutationTable[65] =
+
+static const int keyWithParities[64];// 초기 설정 64비트 키값 이값은 직접 설정하는 값이다.
+
+static const int InitialPermutationTable[64] =
 {
-    0,
     58, 50, 42, 34, 26,18,10, 2,
     60, 52, 44, 36, 28, 20, 12, 4,
     62, 54, 46, 38, 30, 22, 14, 6,
@@ -17,9 +17,8 @@ static const int InitialPermutationTable[65] =
     63, 55, 47, 39, 31, 23, 15, 7
 };
 //마지막 P BOX
-static const int FinalPermutationTable[65]=
+static const int FinalPermutationTable[64]=
 {
-    0,
     40, 8, 48, 16, 56, 24, 64, 32,
     39, 7, 47, 15, 55, 23, 63, 31,
     38, 6, 46, 14, 54, 22, 62, 30,
@@ -31,7 +30,7 @@ static const int FinalPermutationTable[65]=
 };
 
 //라운드 내부
-static const int P_BOX_EX[48]=
+static const int ExpansionPermutationTable[48]=
 {
     32, 1, 2, 3, 4, 5,
     4, 5, 6, 7, 8, 9,
@@ -44,33 +43,38 @@ static const int P_BOX_EX[48]=
 };
 
 
-static const int S_BOX[8][4][16]=
+static const int SubstituteTables[8][4][16]=
 {
-    {   {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
+    {   
+        {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
         {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
         {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
         {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 2, 14, 10, 0, 6, 13}
     },
 
-    {   {15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
+    {   
+        {15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
         {3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
         {0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
         {13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}
     },
 
-    {   {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
+    {   
+        {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
         {13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
         {13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
         {1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12}
     },
 
-    {   {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
+    {   
+        {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
         {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
         {10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
         {3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14}
     },
 
-    {   {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
+    {   
+        {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
         {14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
         {4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
         {11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3}
@@ -82,71 +86,106 @@ static const int S_BOX[8][4][16]=
         {6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12}
     },
 
-    {   {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
+    {   
+        {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
         {10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
         {9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
         {4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13}
     },
 
-    {   {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
+    {   
+        {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
         {1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2},
         {7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8},
         {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}
     }
 };
 
-static const bitset<48> KEY[16];
-
-static int MAT_PERMU[33]=
+static int KeyCompressionTable[32]=
 {
-    0,
     16, 7, 20, 21, 29, 12, 28, 17,
     1, 15, 23, 26, 5, 18, 31, 10,
     2, 8, 24, 14, 32, 27, 3, 9,
     19, 13, 30, 6, 22, 11, 4, 25
 };
 
+
+static const int RoundKeys[16][48];
+
+static const int ParityDropTable[64]=
+{
+    57,49,41,33,25,17,9,1,
+    58,50,42,34,26,18,10,2,
+    59,51,43,35,27,19,11,3,
+    60,52,44,36,63,55,47,39,
+    31,23,15,7,62,54,46,38,
+    30,22,14,6,61,53,45,37,
+    29,21,13,5,28,20,12,4
+};
+
+static const int ShiftTable[16] =
+{ 1,1,2,2,
+2,2,2,2,
+1,2,2,2,
+2,2,2,1}
 ///////////////////////////////////////////////////////////////////////////
 
 void Cipher (int plainBlock[64], int RoundKeys[16, 48], int cipherBlock[64])
 {
-    permute(64, 64, plainBlock, inBlock, InitialPermutationTable);
+    int leftBlock[32] ,rightBlock[32];
+    int inBlock[64];
+
+    permute(64, 64, plainBlock, inBlock, InitialPermutationTable); //plainblock이 pbox를 통해서 inblock 으로 옮긴다 
+
     split (64, 32, inBlock, leftBlock, rightBlock);
-    for (round = 1 to 16)
+
+    for (int round = 1 ; round <= 16; round++)
     {
         mixer (leftBlock, rightBlock, RoundKeys[round]);
-        if    (round!=16)
+        if(round != 16)
             swapper (leftBlock, rightBlock);
         }
+
     combine (32, 64, leftBlock, rightBlock, outBlock);
+
     permute(64, 64, outBlock, cipherBlock, FinalPermutationTable);
+
 }
+
+
 void mixer (leftBlock[48], rightBlock[48], RoundKey[48])
 {
     copy (32, rightBlock, T1);
-    function (T1, RoundKey,T2)exclusiveOr (32, leftBlock, T2, T3);
+    function (T1, RoundKey,T2);
+    exclusiveOr (32, leftBlock, T2, T3);
     copy (32, T3, rightBlock);
 }
+
+
 void swapper (leftBlock[32], rigthBlock[32])
 {
     copy(32, leftBlock, T);
     copy(32, rightBlock, leftBlock);
     copy(32, T,  rightBlock);
 }
+
+
 void function (inBlock[32], RoundKey[48], outBlock[32])
 {
     permute (32, 48, inBlock, T1, ExpansionPermutationTable);
-    exclusiveOr (48, T1, RoundKey,T2);
+    exclusiveOr (48, T1, RoundKey,T2);  
     substitute (T2, T3, SubstituteTables);
     permute    (32, 32, T3, outBlock, StraightPermutationTable);
 }
+
+
 void substitute (inBlock[32], outBlock[48], SubstitutionTables[8, 4, 16])
 {
-    for (i = 1 to 8)
+    for (int i = 0 ; i <  8 ; i++)
     {
         row  = 2  inBlock[i 6 + 1] + inBlock [i * 6 + 6];
         col  = 8 * inBlock[i * 6 + 2] + 4 * inBlock[i * 6 + 3] +2*inBlock[i * 6 + 4] + inBlock[i*6 + 5];
-        value    =    SubstitutionTables [i][row][col];
+        value    =    SubstitutionTables[i][row][col];
         outBlock[i * 4 + 1]  = value / 8;
         value = value % 8;
         outBlock[i* 4 + 2]  = value / 4;
@@ -156,27 +195,72 @@ void substitute (inBlock[32], outBlock[48], SubstitutionTables[8, 4, 16])
         outBloc[i*4 + 4] = value;
     }
 }
+
+
+void split (int com_num, int spl_num, int  inBlock[], int leftBlock[],int rightBlock[]) //inblock을 왼 오 로 나눈다.
+{
+    for(int i = 0 ; i < spl_num ; i++ )
+    {
+        rightBlock[i] = inBlock[i];
+        leftBlock[i] = inBlock[i+spl_num];
+    }
+}
+
+void permute(int first_Block_num, int final_Block_num, int firstBlock[], int finalBlock[], int Table[])
+{
+    for(int i = 0 ; i < final_Block_num ; i++)
+    {
+        finalBlock[i] = firstBlock[Table[i]-1];
+    }
+}
+
+void combine (int spl_num, int com_num,int leftBlock[],int rightBlock,int outBlock[])
+{
+      for(int i = 0 ; i < spl_num ; i++ )
+    {
+        inBlock[i] = rightBlock[i] ;
+        inBlock[i+spl_num] = leftBlock[i] ;
+    }
+}
+
+void copy(int num ,int rightBlock[],int T1[])
+{
+    for(int i = 0 ; i < num ; i++)
+    {
+        T1[i] = rightBlock[i];
+    }
+}
+
+void exclusiveOr (int num,int T1[],int RoundKey[],int T2[]) //T1과 rountdey를 xor해서 T2에 저장한다.
+{
+    for(int i = 0 ; i < num ; i++)
+    {
+        T2[i] = T1[i] ^ RoundKeys[i] ;
+    }
+}
+
+
 ///////////////////////////////////////////////////////////////////////////
 //키생성
-void Key_Generator (keyWithParities[64], RoundKeys[16, 48], ShiftTable[16])
+void Key_Generator(int keyWithParities[],int RoundKeys[16, 48],int ShiftTable[])
 {
-    permute (64, 56, keyWithParities, cipherKey, ParityDropTable);
-    split (56, 28, cipherKey, leftKey, rightKey);
-    for (round = 1 to 16)
+    permute(64, 56, keyWithParities, cipherKey, ParityDropTable); //축소 P박스 
+    split(56, 28, cipherKey, leftKey, rightKey);
+    for (int round = 0 ;round < 16; i++)
     {
-        shiftLeft(leftKey,    ShiftTable[round]);
-        shiftLeft(rightKey,    ShiftTable[round]);
+        shiftLeft(leftKey,ShiftTable[round]);
+        shiftLeft(rightKey,ShiftTable[round]);
         combine(28, 56, leftKey, rightKey, preRoundKey);
         permute(56, 48, preRoundKey, RoundKeys[round], KeyCompressionTable);
     }
 }
 
-void shiftLeft (block[28], numOfShifts)
+void shiftLeft(int block[28], int numOfShifts)
 {
-    for (i = 1 to numOfShifts)
+    for (int i = 1 ; i <= numOfShifts; i++)
     {
         T = block[1];
-        for (j = 2 to 28)
+        for (int j = 2  ;j<= 28 ; j++)
         {
             block[j−1] = block[j];
         }
